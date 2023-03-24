b1af4a2335f5bce4ec6f168c42029f948592a7eb
Joshua Jumelet <joshuajumelet@gmail.com>
2023-03-24 15:56:20 -0700

    build UserRouter and routes

---
+++ routes/UserRouter.js
@@ -0,0 +1,44 @@
+const Router = require('express').Router()
+const controller = require('../controllers/UserController')
+const middleware = require('../middleware')
+
+Router.get(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.GetUsers
+)
+
+Router.get(
+  '/session',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CheckSession
+)
+
+Router.get(
+  '/details/:user_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.GetUserById
+)
+
+Router.post('/register', controller.RegisterUser)
+
+Router.post('/login', controller.LoginUser)
+
+Router.put(
+  '/update_password/:user_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdatePassword
+)
+
+Router.delete(
+  '/delete/:user_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteUser
+)
+
+module.exports = Router
