13726e6a2e016684fcb1667e9798184c7e4cbc88
Joshua Jumelet <joshuajumelet@gmail.com>
2023-03-28 08:06:41 -0700

    build OrderRouter and updated AppRouter with route details

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,8 +1,10 @@
 const Router = require('express').Router()
 const UserRouter = require('./UserRouter')
 const ProductRouter = require('./ProductRouter')
+const OrderRouter = require('./OrderRouter')
 
 Router.use('/users', UserRouter)
 Router.use('/products', ProductRouter)
+Router.use('/orders', OrderRouter)
 
 module.exports = Router
---
+++ routes/OrderRouter.js
@@ -0,0 +1,47 @@
+const Router = require('express').Router()
+const controller = require('../controllers/OrderController')
+const middleware = require('../middleware')
+
+Router.get(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.GetOrders
+)
+
+Router.get(
+  '/:order_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.GetOrderDetails
+)
+
+Router.get(
+  '/:user_id/orders',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.GetUserOrders
+)
+
+Router.post(
+  '/create_order',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateOrder
+)
+
+Router.put(
+  '/:order_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdateOrder
+)
+
+Router.delete(
+  '/:order_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteOrder
+)
+
+module.exports = Router
