de82c672bf4034539b439874e58c3322c7b4b38f
Joshua Jumelet <joshuajumelet@gmail.com>
2023-03-28 14:10:54 -0700

    build Register component

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,123 @@
+import { useState } from 'react'
+import { RegisterUser } from '../services/Auth'
+import { useNavigate } from 'react-router-dom'
+
+const Register = ({}) => {
+  let navigate = useNavigate()
+
+  let initialState = {
+    name: '',
+    email: '',
+    address: '',
+    password: '',
+    confirmPassword: ''
+  }
+
+  const [formValues, setFormValues] = useState(initialState)
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await RegisterUser({
+      name: formValues.name,
+      email: formValues.email,
+      address: formValues.address,
+      password: formValues.password
+    })
+    setFormValues(initialState)
+    navigate('/login')
+  }
+
+  return (
+    <div className='register'>
+      <div className='register-card'>
+        <form className='register-form' onSubmit={handleSubmit}>
+          <div className='input-wrapper'>
+            <label className='label' htmlFor='name'>
+              Name:
+            </label>
+            <input
+              className='reg'
+              onChange={handleChange}
+              name='name'
+              type='text'
+              placeholder='John Smith'
+              value={formValues.name}
+              required
+            />
+            <label className='label' htmlFor='email'>
+              Email:
+            </label>
+            <input
+              className='reg'
+              onChange={handleChange}
+              name='email'
+              type='text'
+              placeholder='example@email.coom'
+              value={formValues.email}
+              required
+            />
+            <label className='label' htmlFor='address'>
+              Address:
+            </label>
+            <input
+              className='reg'
+              onChange={handleChange}
+              name='address'
+              type='text'
+              placeholder='Enter address here'
+              value={formValues.address}
+              required
+            />
+            <label className='label' htmlFor='password'>
+              Password:
+            </label>
+            <input
+              className='reg'
+              onChange={handleChange}
+              name='password'
+              type='text'
+              value={formValues.password}
+              required
+            />
+            <label className='label' htmlFor='password'>
+              Confirm Password:
+            </label>
+            <input
+              className='reg'
+              onChange={handleChange}
+              name='confirmPassword'
+              type='text'
+              value={formValues.confirmPassword}
+              required
+            />
+          </div>
+          <div className="loginbutton">
+            <button
+              className="loginbtn"
+              disabled={
+                !formValues.email ||
+                (!formValues.password &&
+                  formValues.confirmPassword === formValues.password)
+              }
+            >
+              Login
+            </button>
+            <p className="account">
+              Already have an account? Click{' '}
+              <button className="switchbtn" onClick={() => setShowing(false)}>
+                here
+              </button>{' '}
+              to Login!
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Register
