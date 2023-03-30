10002e3cb2126d631f447f0a8f76d8a260b01b65
Joshua Jumelet <joshuajumelet@gmail.com>
2023-03-30 09:45:08 -0700

    build initial route setup in App.js / build About page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,51 @@
 import './App.css'
+import axios from 'axios'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { CheckSession } from './services/Auth'
+import Nav from './components/Nav'
+import Register from './components/Register'
+import Login from './components/Login'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
-  return <div className="App"></div>
+  let navigate = useNavigate()
+
+  const [user, setUser] = useState(null)
+  const [showing, setShowing] = useState(false)
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setuser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
+  return (
+    <div className="App">
+      <div>
+        <Nav />
+      </div>
+      <main>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route
+          path="/login"
+          element={<Login setUser={setUser} setShowing={setShowing} />}
+        />
+        <Route
+          path="/register"
+          element={<Register setShowing={setShowing} />}
+        />
+      </main>
+    </div>
+  )
 }
 
 export default App
---
+++ client/src/pages/About.js
@@ -0,0 +1,50 @@
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About Poké Mart</h1>
+      <span>
+        <p>
+          Poké Mart is a E-Commerce platform for purchasing items from the world
+          of Pokémon. It was designed as a modern application that replicates a
+          online store where a user can search through available products and
+          make orders. To add to cart a user is required to create a User Auth
+          account that offers security to their personal information.
+        </p>
+        <p>Please come again!</p>
+      </span>
+      <div className="about-us">
+        <div className="Josh-profile">
+          <img
+            className="josh"
+            src={'https://i.imgur.com/rtAwtJu.jpg'}
+            alt=""
+          />
+          <a
+            className="josh-git"
+            href="https://github.com/joshjumelet"
+            target="blank"
+          >
+            <img
+              className="github"
+              style={{ width: 30, height: 30 }}
+              src="https://i.imgur.com/AjmoOeq.png"
+              alt="github"
+            />
+          </a>
+          <a
+            className="josh-linkedIn"
+            href="https://www.linkedin.com/in/joshua-jumelet/"
+            target="blank"
+          >
+            <img
+              className="linkedIn"
+              style={{ width: 30, height: 30 }}
+              src="https://i.imgur.com/N4ceP5A.png"
+              alt="linkedIn"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
