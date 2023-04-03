e7a289a1a20eaec1006dd5ce854a57664a4a7440
Joshua Jumelet <joshuajumelet@gmail.com>
2023-04-02 17:15:55 -0700

    compleete AddProduct functionality

--- client/src/pages/AddProduct.js
+++ client/src/pages/AddProduct.js
@@ -10,7 +10,64 @@ const AddProduct = ({ allProducts }) => {
     price: ''
   }
 
-  return <div></div>
+  const [newProduct, setNewProduct] = useState(initProduct)
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await Client.post('/api/products/create', newPark)
+      setNewProduct(initProduct)
+      allProducts()
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleChange = (e) => {
+    setNewProduct({ ...newProduct, [e.target.id]: e.target.value })
+  }
+
+  return (
+    <div>
+      <h1>Add A New Product</h1>
+      <h3>Please fill in the product info</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          onChange={handleChange}
+          value={newProduct.name}
+        />
+        <label htmlFor="image">Image Url:</label>
+        <input
+          type="text"
+          id="image"
+          onChange={handleChange}
+          value={newProduct.image}
+        />
+        <label htmlFor="description">Description:</label>
+        <input
+          type="text"
+          id="description"
+          onChange={handleChange}
+          value={newProduct.description}
+        />
+        <label htmlFor="price">Price:</label>
+        <input
+          type="text"
+          id="price"
+          onChange={handleChange}
+          value={newProduct.price}
+        />
+        <button type="submit" className="button">
+          Add Product
+        </button>
+      </form>
+    </div>
+  )
 }
 
 export default AddProduct
